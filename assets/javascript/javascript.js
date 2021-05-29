4f3d088967d32596814b3d4fb52fc477d0c5a3f4
kristjanhenriroots <kristjan@roots.ee>
2021-05-29 13:06:10 +0300

    fix code style

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,41 +1,41 @@
 // Get the modal
-var modal = document.getElementById("modal")
+var modal = document.getElementById('modal')
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var images = document.getElementsByClassName("photo")
+// Get the image and insert it inside the modal - use its 'alt' text as a caption
+var images = document.getElementsByClassName('photo')
 
 var urls = []
 var captions = []
-for(var i = 0; i < images.length; i++) {
-  urls.push(images[i].src.replace(".crop", ""))
+
+for (var i = 0; i < images.length; i++) {
+  urls.push(images[i].src.replace('.crop', ''))
   captions.push(images[i].alt)
 
-    images[i].onclick = function() {
-        var modalImg = document.getElementById("modal-img")
-        var captionText = document.getElementById("modal-caption")
-        
-        modal.style.display = "block"
-        modalImg.src = this.src.replace(".crop", "")
-        captionText.innerHTML = this.alt
-    }
-}
-console.log(urls)
+  images[i].onclick = function () {
+    var modalImg = document.getElementById('modal-img')
+    var captionText = document.getElementById('modal-caption')
 
-document.getElementById("modal-close").onclick = function() {
-  modal.style.display = "none"
+    modal.style.display = 'block'
+    modalImg.src = this.src.replace('.crop', '')
+    captionText.innerHTML = this.alt
+  }
 }
 
-document.onkeydown = function(evt) {
+document.getElementById('modal-close').onclick = function () {
+  modal.style.display = 'none'
+}
+
+document.onkeydown = function (evt) {
   evt = evt || window.event
 
   var isEscape = false
   var isRight = false
 
-  if ("key" in evt) {
+  if ('key' in evt) {
     console.log(evt.key);
-    isEscape = evt.key === "Escape" || evt.key === "Esc"
-    isRight = evt.key === "ArrowRight"
-    isLeft = evt.key === "ArrowLeft"
+    isEscape = evt.key === 'Escape' || evt.key === 'Esc'
+    isRight = evt.key === 'ArrowRight'
+    isLeft = evt.key === 'ArrowLeft'
   } else {
     isEscape = evt.keyCode === 27
     isRight = evt.keyCode === 39
@@ -43,26 +43,26 @@ document.onkeydown = function(evt) {
   }
 
   if (isEscape) {
-    modal.style.display = "none"
+    modal.style.display = 'none'
   }
+
   if (isRight) {
-    console.log("blah")
-    var current = document.getElementById("modal-img").src
+    var current = document.getElementById('modal-img').src
     var a = urls.indexOf(current)
-    console.log(a, urls.length);
-    if (a < urls.length-1) {
-      document.getElementById("modal-img").src = urls[a+1]
-      document.getElementById("modal-caption").innerHTML = captions[a+1]
-      console.log(captions);
-       
+
+    if (a < urls.length - 1) {
+      document.getElementById('modal-img').src = urls[a + 1]
+      document.getElementById('modal-caption').innerHTML = captions[a + 1]
     }
   }
+
   if (isLeft) {
-    var current = document.getElementById("modal-img").src
+    var current = document.getElementById('modal-img').src
     var a = urls.indexOf(current)
+
     if (a !== 0) {
-      document.getElementById("modal-img").src = urls[a-1]
-      document.getElementById("modal-caption").innerHTML = captions[a-1] 
+      document.getElementById('modal-img').src = urls[a - 1]
+      document.getElementById('modal-caption').innerHTML = captions[a - 1]
     }
   }
 }
